3ee96d128e32858d77439365644411ef4fc4b8a5
mmusil2 <mmusil2@kent.edu>
2019-02-12 14:55:25 -0500

    added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,19 @@ var keys = require("../keys");
 
 var password =  keys.mysql.password;
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: password,
     database: "burgers_db"
-});
+  });
+};
 
 connection.connect(function(err) {
     if (err) {
